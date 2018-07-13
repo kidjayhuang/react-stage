c4273c73439a6f325c1e25eed8adfcbcab8ecd2c
huangjiehong <huangjiehong@zhongan.com>
2018-07-13 10:35:08 +0800

    init

---
+++ src/home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+export default class Home extends React.Component {
+    componentWillMount() {
+        console.log(1111)
+    }
+    render(){
+        return (
+            <div className="home-page">11111</div>
+        )
+    }
+}
+  
